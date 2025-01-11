881fdedc7b5c4ad0f5e0b9a1d62e2300d0ef57ea
abdndmn <abdndmn@hotmail.com>
2025-01-11 05:16:44 +0300

    Add project files.

---
+++ PatikaLibrary/Books.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaLibrary
+{
+    internal class Books//Class
+    {
+        
+        public string BookName { get; set; }//Property:
+        public string AuthorName { get; set; }
+        public string AuthorSurname { get; set;}
+        public int PageNumber { get; set; }
+        public string Publisher { get; set; }
+        public DateTime RegisterDate { get; set; }
+
+        public Books()//Default Constructor
+        {
+            RegisterDate = DateTime.Now;
+        }
+
+        public Books(string bookName, string authorName,string authorSurname,int pageNumber,string publisher)//Parametre alan alternatif constructor
+        {
+            BookName = bookName;
+            AuthorName = authorName;
+            AuthorSurname = authorSurname;
+            PageNumber = pageNumber;
+            Publisher = publisher;
+                
+            RegisterDate= DateTime.Now;
+
+        }
+    }
+}
---
+++ PatikaLibrary/Program.cs
@@ -0,0 +1,16 @@
+﻿using PatikaLibrary;
+
+Books book1 = new Books();//New,Nesne olusturma, Default Constructor kullanıldı.
+
+book1.BookName = "Adı Aylin";
+book1.AuthorName = "Ayşe";
+book1.AuthorSurname = "Kulin";
+book1.PageNumber = 398;
+book1.Publisher = "Remzi Kitabevi";
+book1.RegisterDate = DateTime.Now;
+
+Console.WriteLine("Kitap Adı: "+book1.BookName+"\nYazar Adı, Soyadı : "+book1.AuthorName+" "+book1.AuthorSurname+"\nSayfa Sayısı: "+book1.PageNumber+"\nYayınevi: "+book1.Publisher+"\nKayıt Tarihi: "+book1.RegisterDate);
+
+Books book2 = new Books("Adı Aylin","Ayşe","Kulin",398,"Remzi Kitabevi");//New, Alternatif constructor kullanarak nesne olusturma 
+
+Console.WriteLine("Kitap Adı: " + book2.BookName + "\nYazar Adı, Soyadı : " + book2.AuthorName + " " + book2.AuthorSurname + "\nSayfa Sayısı: " + book2.PageNumber + "\nYayınevi: " + book2.Publisher + "\nKayıt Tarihi: " + book2.RegisterDate);
